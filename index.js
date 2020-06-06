e8090b1cf53c477f6ab9a74ff072d3edc2f3d2af
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-06-06 09:46:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("home");
-});
-app.get("/about", function(req, res){
-    res.render("about");
-});
-app.get("/contact", function(req, res){
-    res.render("contact");
 });
 app.get("*", function(req, res){
     res.render("404");
