da11f804d29a40c6cff052b140a1a5fcbc951364
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-07-11 06:27:17 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const routes = require("./routes/index");
+
+const User = require("./models/user");
 
 const app = express();
 
@@ -13,7 +16,40 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-// app.use('/', routes);
+/*
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(methodOverride('_method'));
+
+app.use(cookieParser('secret'));
+
+app.use(require('express-session')({
+    secret : '',
+    resave : false,
+    saveUninitialized : false
+}));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.success = req.flash('success');
+   res.locals.error = req.flash('error');
+   next();
+});
+
+app.use('/', routes);
+
+*/
 
 app.get("/", function(req, res){
     res.render("home");
